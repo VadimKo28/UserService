10d2f12ad4c93a221cf230cc14b32d7ce0809c8f
vadim_kozelsky <vadim.kozelskij@mail.ru>
2026-01-08 20:39:57 +0300

    Add logger

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,43 @@ package main
 
 import (
 	"log"
+	"log/slog"
+	"os"
 	"user_advt/internal/config"
 )
 
-func main() {
-	config.MustLoad()
+const (
+	envLocal	= "local"
+	envProd	= "prod"
+	envDev	= "dev"
+)
 
+func main() {
+	cfg := config.MustLoad()
 	log.Println("Config loaded")
 
+	logger := LoggerSetup(cfg.Env)
+	logger.Info("Init app", slog.String("env:", cfg.Env))
+
+}
+
+func LoggerSetup(env string) *slog.Logger {
+	var log *slog.Logger
+
+  switch env {
+    case envLocal:
+			log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}))
+    case envDev:
+			log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}))
+		case envProd:
+			log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			}))
+  }
+
+	return log 
 }
